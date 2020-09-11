629d29fdb8784fcaee56c703836d81b471dc9cea
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2020-09-11 10:31:13 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './COMPONENT/UNCHANGED/footer/footer.component';
 import { HeaderComponent } from './COMPONENT/UNCHANGED/header/header.component';
-import { SlideComponent } from './COMPONENT/CHANGE/slide/slide.component';
 import { HomeComponent } from './COMPONENT/CHANGE/home/home.component';
 import { ProductComponent } from './COMPONENT/CHANGE/product/product.component';
 import { CategoriesComponent } from './COMPONENT/CHANGE/categories/categories.component';
@@ -15,7 +14,6 @@ import { DetailComponent } from './COMPONENT/CHANGE/product/detail/detail.compon
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SlideComponent,
     HomeComponent,
     CategoriesComponent,
   ],
